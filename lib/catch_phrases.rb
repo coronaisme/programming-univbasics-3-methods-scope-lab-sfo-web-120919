9670be51da540c44c3bd0f40d43c0def47b374b8
Nick Corona <coronaisme@gmail.com>
2019-11-28 02:37:10 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,9 @@
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 def mario
   phrase = "It's-a me, Mario!"
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
 def toadstool
-  puts status
+  puts $status
 end
